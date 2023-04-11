654b08c39da268763e9356cea43c08a2936b7005
Diana Villarreal <dianacarolinav97@gmail.com>
2023-04-11 18:02:27 -0500

    second commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className='Navbar'>
+            <h1>Diana
+                <br /> 
+                Villarreal</h1>
+            <ul className='navbar_list'>
+                <li>ABOUT</li>
+                <li>PROJECTS</li>
+                <li>CONTACT</li>
+            </ul>
+            
+        </div>
+    );
+};
+
+export default NavBar;

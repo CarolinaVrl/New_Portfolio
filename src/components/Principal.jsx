783752bb9fb70213d0dded3cfcf0832c26bd6d1b
Diana Villarreal <dianacarolinav97@gmail.com>
2023-04-17 12:27:41 -0500

    portfolio

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -7,50 +7,59 @@ import tecnologys from '../assets/tecnologys';
 const Principal = () => {
     return (
         <div className='principal'>
-            <section className='inicio'>
-                <h2 >Hola! <span style={{color:'white'}}> Soy</span> </h2>
-                <h1>Diana Carolina <br /> Villarreal</h1>
-                <h2 style={{color:'white'}}>Desarrolladora FrontEnd</h2>
-                <div>
-                    <button className='dowload'>Descargar CV</button>
-                    <button>Sobre mí</button>
-                </div>
+            <div className='together'>
 
-            </section>
-            <section className='home'>
-                <div className='home_position'>
-                    <div className='home_photo'>
-                        <img src={diana} alt="" />
-                    </div>
-                    <div className='home_media'>
-                        <a href=""><i className="fa-brands fa-linkedin-in"></i></a>
-                        <a href=""><i className="fa-brands fa-whatsapp"></i></a>
-                        <a href=""><i className="fa-brands fa-github"></i></a>
 
+                <section className='inicio'>
+                    <h2 >Hola! <span style={{ color: 'white' }}> Soy</span> </h2>
+                    <h1>Diana Carolina <br /> Villarreal</h1>
+                    <h2 style={{ color: 'white' }}>Desarrolladora FrontEnd</h2>
+                    <div>
+                        <button className='dowload'>Descargar CV</button>
+                        <button>Sobre mí</button>
                     </div>
 
-                </div>
-            </section>
+                </section>
+                <section className='home'>
+                    <div className='home_position'>
+                        <div className='home_photo'>
+                            <img src={diana} alt="" />
+                        </div>
+                        <div className='home_media'>
+                            <a href=""><i className="fa-brands fa-linkedin-in"></i></a>
+                            <a href=""><i className="fa-brands fa-whatsapp"></i></a>
+                            <a href=""><i className="fa-brands fa-github"></i></a>
+
+                        </div>
+
+                    </div>
+                </section>
+            </div>
             <section className='aboutMe'>
                 <h2>Sobre mí</h2>
-                <div className='about_photo'>
-                    <img src={diana2} alt="" />
+                <div className='about_container'>
+                    <div className='about_photo'>
+                        <img src={diana2} alt="" />
+
+                    </div>
+                    <div className='about_text'>
+                        <p>
+                            Soy una programadora con habilidades en JavaScript usando la librería de React.
+                            Manejando framework como Bootstrap, Tailwind y Angular.
+
+
+                            Soy una persona responsable que disfruta el trabajar en equipo y aprender cosas nuevas en este mundo de la tecnología
+
+                        </p>
+                        <h3>¡Me encantaría trabajar para tu empresa y cumplir juntos nuevos objetivos!</h3>
+                        <button>Contáctame!</button>
+
+                    </div>
+
+                    
 
                 </div>
-                <div className='about_text'>
-                    <p>
-                        Soy una programadora con habilidades en JavaScript usando la librería de React.
-                        Manejando framework como Bootstrap, Tailwind y Angular.
 
-
-                        Soy una persona responsable que disfruta el trabajar en equipo y aprender cosas nuevas en este mundo de la tecnología
-
-                    </p>
-                    <h3>¡Me encantaría trabajar para tu empresa y cumplir juntos nuevos objetivos!</h3>
-
-                </div>
-
-                <button>Contáctame</button>
             </section>
             <section className='exp'>
                 <h2>Mi Experiencia</h2>
@@ -158,21 +167,21 @@ const Principal = () => {
                     </ul>
                 </div> */}
                 <div className='proyects_container_main'>
-                {
-                    projects.map(item => (
-                        <div className='proyects_card'>
-                            <img src={item.img} alt="" />
-                            <div className='proyects_text'>
-                                <h2>{item.name}</h2>
-                                <a href={item.url} target='_blank'>Sitio Web</a>
+                    {
+                        projects.map(item => (
+                            <div className='proyects_card'>
+                                <img src={item.img} alt="" />
+                                <div className='proyects_text'>
+                                    <h2>{item.name}</h2>
+                                    <a href={item.url} target='_blank'>Sitio Web</a>
+                                </div>
+
                             </div>
+                        ))
 
-                        </div>
-                    ))
-
-                }
+                    }
                 </div>
-               
+
 
             </section>
             <section className='contact'>

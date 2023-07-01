a73696fe5ed517910f2c6c48803eb5b5f4805739
Diana Villarreal <dianacarolinav97@gmail.com>
2023-07-01 13:16:14 -0500

    feat: onlick function

--- src/components/Proyects.tsx
+++ src/components/Proyects.tsx
@@ -4,45 +4,48 @@ import projects from '../assets/proyects'
 const Proyects = () => {
 
 
-    const allCategories = ['All',...new Set(projects.map(item=>item.category))]
-    const [categories, setCategories]= useState(allCategories)
-    const [project, setProject]=useState(projects)
+    const allCategories = ['All', ...new Set(projects.map(item => item.category))]
+    const [categories, setCategories] = useState(allCategories)
+    const [project, setProject] = useState(projects)
 
-    const filterCategory=(caategory)=>{
-        console.log(caategory)
+    const filterCategory = (category) => {
+        console.log(category)
     }
 
     return (
         <section id='proyects' className='proyects'>
-        <h2 className='title'>Proyectos</h2>
-        <div className='proyects_filter'>
-            {
-                categories.map(item=>(
-                    <button className='btn-filter'>{item}</button>
-                ))
-            }
-            
-            
-            
-        </div>
-        <div className='proyects_container_main'>
-            {
-                projects.map(item => (
-                    <div className='proyects_card'>
-                        <img src={item.img} alt="" />
-                        <div className='proyects_text'>
-                            <h2>{item.name}</h2>
-                            <a href={item.url} target='_blank'>Sitio Web</a>
+            <h2 className='title'>Proyectos</h2>
+            <div className='proyects_filter'>
+                {
+                    categories.map(item => (
+                        <button
+                            type='button'
+                            onClick={()=>filterCategory}
+                            className='btn-filter'>{item}</button>
+                    ))
+                }
+
+
+
+            </div>
+            <div className='proyects_container_main'>
+                {
+                    projects.map(item => (
+                        <div className='proyects_card'>
+                            <img src={item.img} alt="" />
+                            <div className='proyects_text'>
+                                <h2>{item.name}</h2>
+                                <a href={item.url} target='_blank'>Sitio Web</a>
+                            </div>
+
                         </div>
+                    ))
 
-                    </div>
-                ))
-
-            }
-        </div>
+                }
+            </div>
 
 
-    </section>
+        </section>
     );
 };
 

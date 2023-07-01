17e9f1f1008aea269bdaf534169086f232207983
Diana Villarreal <dianacarolinav97@gmail.com>
2023-07-01 11:53:15 -0500

    feat: add property category

--- src/assets/proyects.js
+++ src/assets/proyects.js
@@ -1,78 +1,60 @@
-import React from 'react';
-import pokedex from './Proyectos/pokedexPc.png';
-import ecommerceJs from './Proyectos/ecommercejsCel.png';
-import ecommerceReact from './Proyectos/ecommerceReactPc.png';
-import phrasesRandom from './Proyectos/phrasesRandom.png';
-import qr from './Proyectos/qrcel.png';
-import crud from './Proyectos/Crud.png';
-import results from './Proyectos/results.png'
-import pres from './Proyectos/pres.png'
-
-
+import React from "react";
+import pokedex from "./Proyectos/pokedexPc.png";
+import ecommerceJs from "./Proyectos/ecommercejsCel.png";
+import ecommerceReact from "./Proyectos/ecommerceReactPc.png";
+import phrasesRandom from "./Proyectos/phrasesRandom.png";
+import qr from "./Proyectos/qrcel.png";
+import crud from "./Proyectos/Crud.png";
+import results from "./Proyectos/results.png";
+import pres from "./Proyectos/pres.png";
 
 export default [
-    {
-        name: 'Pokedex',
-        img: pokedex,
-        url:'https://pokedex-villarreal.netlify.app'
-
-
-    },
-    // goAndStay={
-    //     name:'GoAndStay'
-
-
-
-    // },
-    // PrestiYa={
-    //     name:
-    //     img:
-
-
-    // },
-    {
-        name: 'Lista de Usuarios',
-        img: crud,
-        url:'https://warm-sherbet-f40739.netlify.app'
-
-
-    },
-    {
-        name: 'Ecommerce',
-        img: ecommerceReact,
-        url:'https://eccomerce-react-tvs.netlify.app'
-
-
-    },
-    {
-        name: 'Ecommerce Academlo',
-        img: ecommerceJs,
-        url:'https://peppy-frangollo-f76b91.netlify.app'
-
-
-    },
-    {
-        name: 'Qr',
-        img: qr,
-        url:'https://playful-beignet-2d3d52.netlify.app'
-
-
-    },
-    {
-        name: 'Frases Random',
-        img: phrasesRandom,
-        url:'https://random-phrases-6d755a.netlify.app'
-
-    },
-    {
-        name:'Resultados',
-        img: results,
-        url:'https://result-htmlcss.netlify.app'
-    },
-    {
-        name:'Pres',
-        img: pres,
-        url:'https://credig-testangular.web.app'
-
-    }
-]
+  {
+    name: "Pokedex",
+    img: pokedex,
+    url: "https://pokedex-villarreal.netlify.app",
+    category:'react'
+  },
+  {
+    name: "Lista de Usuarios",
+    img: crud,
+    url: "https://warm-sherbet-f40739.netlify.app",
+    category:'react'
+  },
+  {
+    name: "Ecommerce",
+    img: ecommerceReact,
+    url: "https://eccomerce-react-tvs.netlify.app",
+    category:'react'
+  },
+  {
+    name: "Ecommerce Academlo",
+    img: ecommerceJs,
+    url: "https://peppy-frangollo-f76b91.netlify.app",
+    category:'javascript'
+  },
+  {
+    name: "Qr",
+    img: qr,
+    url: "https://playful-beignet-2d3d52.netlify.app",
+    category:'css'
+  },
+  {
+    name: "Frases Random",
+    img: phrasesRandom,
+    url: "https://random-phrases-6d755a.netlify.app",
+    category:'react'
+  },
+  {
+    name: "Resultados",
+    img: results,
+    url: "https://result-htmlcss.netlify.app",
+    category:'css'
+  },
+  {
+    name: "Pres",
+    img: pres,
+    url: "https://credig-testangular.web.app",
+    category:'angular'
+  },
+];
